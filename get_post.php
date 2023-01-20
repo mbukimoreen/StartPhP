5f2702355d8145dac8290e500559fb8f13894c98
unknown <you@example.com>
2023-01-20 15:14:34 +0300

    updated Form in the GET doc.

--- get_post.php
+++ get_post.php
@@ -4,6 +4,10 @@
 /*
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
+
+ echo " This is simple to capture your information which will be used to populate your CV";
+ echo "<br>";
+ echo "<br>";
 ?> 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,11 +23,27 @@
 <br><br>
 
   <form action="<?php echo $_SERVER["PHP_SELF"] ; ?><" method="post"></form>
+    <label for="First name"> First name:</label>
+    <input type="text" name="first name" id="firstName">
+    <br> <br>
+    <label for="Middle name">Middle name:</label>
+    <input type="text" name="middle name" id=" middleName">
+    <br> <br>
+    <label for="Phone No">Phone No:</label>
+    <input type="number" name="phone no" id="phoneNo">
+    <br> <br>
+    <label for="Email">Email:</label>
+    <input type="email" name="email" id="">
+    <br> <br>
     <label for="Username"> Username:</label>
     <input type="text" name="username" id=" userName">
     <br> <br>
+    <label for="Date of birth"> Date of Birth: </label>
+    <input type="date" name="date of birth" id="dateBirth">
+    <br> <br>
     <label for="Password">Password:</label>
     <input type="password" name="password" id="pwd">
+  
     <br> <br>
     <input type="submit" value="Submit">
     <input type="reset" value="Clear Form">
