2e546f1f44d9b4c4063f8e34e2213565080c0976
unknown <you@example.com>
2023-01-24 22:09:30 +0300

    Updated

--- Sessions.php
+++ Sessions.php
@@ -10,6 +10,7 @@
  session_destroy()
 */
 
+/*
 session_start(); // for accessing any session data
 
 if (isset($_POST["submit"])) {
@@ -54,5 +55,67 @@ if (isset($_POST["submit"])) {
     <input type="submit" value="Login ">
 
 </form>
+</body>
+</html>
+*/
+//<?php
+
+/* ------------ Sessions ------------ */
+
+/*
+  Sessions are a way to store information (in variables) to be used across multiple pages.
+  Unlike cookies, sessions are stored on the server.
+*/
+
+session_start(); // Must be called before accessing any session data
+
+if (isset($_POST['submit'])) {
+  $username = filter_input(
+    INPUT_POST,
+    'username',
+    FILTER_SANITIZE_FULL_SPECIAL_CHARS
+  );
+  $password = filter_input(
+    INPUT_POST,
+    'password',
+    FILTER_SANITIZE_FULL_SPECIAL_CHARS
+  );
+
+  if ($username == 'mbukim' && $password == 'password') {
+    // Set Session variable
+    $_SESSION['username'] = $username;
+    // Redirect user to another page
+    header('Location: /startphp/resource/dashboard.php');
+  } else {
+    echo '<h1> Welcome, Guest </h1>';
+    echo '<a href = "home" > Home</a>';
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Session</title>
+</head>
+<body>
+    
+  <form action="<?php echo htmlspecialchars(
+    $_SERVER['PHP_SELF']
+  ); ?>" method="POST">
+    <div>
+      <label>Username: </label>
+      <input type="text" name="username">
+    </div>
+    <br>
+    <div>
+      <label>Password: </label>
+      <input type="password" name="password">
+    </div>
+    <br>
+    <input type="submit" name="submit" value="Submit">
+  </form>
 </body>
 </html>
---
+++ resource/logout.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+
+// destroy the session
+session_destroy();
+
+header('Location: /startphp/Sessions.php');
+
+?>
